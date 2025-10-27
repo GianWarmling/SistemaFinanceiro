7449101849ca92a502029b4da093fde0424ea103
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2025-10-27 20:47:28 -0300

    Criada controller Usuário

---
+++ SistemaFinanceiro/Controllers/UsuariosController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaFinanceiro.Data;
+using SistemaFinanceiro.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SistemaFinanceiro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<UsuariosController>
+        [HttpGet]
+        public async Task<IActionResult> GetUsuarios()
+        {
+            var usuarios = await _context.Usuarios.ToListAsync();
+            return Ok(usuarios);
+        }
+
+        // GET api/<UsuariosController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+            return Ok(usuario);
+        }
+
+        // POST api/<UsuariosController>
+        [HttpPost]
+        public async Task<IActionResult> CriarUsuario(int id, [FromBody] Usuario usuario)
+        {
+            var usuarioDb = await _context.Usuarios.FindAsync(id);
+            if (usuarioDb == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+            usuarioDb.Nome = usuario.Nome;
+            usuarioDb.Email = usuario.Email;
+            await _context.SaveChangesAsync();
+            return Ok(usuarioDb);
+        }
+
+        // PUT api/<UsuariosController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarUsuario(int id, [FromBody] Usuario usuario)
+        {
+            var usuarioDb = await _context.Usuarios.FindAsync(id);
+            if(usuarioDb == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+            usuarioDb.Nome = usuario.Nome;
+            usuarioDb.Email = usuario.Email;
+            await _context.SaveChangesAsync();
+            return Ok(usuarioDb);
+        }
+
+        // DELETE api/<UsuariosController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
